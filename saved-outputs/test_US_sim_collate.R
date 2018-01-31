fdb526c97d1d0d468dcfd37bc190a9306e4ff313
Lucy Li <luli@hsph.harvard.edu>
2018-01-31 10:51:18 -0500

    new output folders

--- saved-outputs/test_US_sim_collate.R
+++ saved-outputs/test_US_sim_collate.R
@@ -3,57 +3,29 @@ library(magrittr)
 library(ggplot2)
 
 
+
 ## Load actual data
 
-data.env <- new.env()
-load("data/united-states/data.RData", data.env)
-under.5.prev.data <- data.env$serotype.prev.list[1:5] %>%
-    lapply(select, -1) %>%
-    lapply(rowSums) %>%
-    Reduce("+", .) %>%
-    data.frame(year=data.env$serotype.prev.list[[1]]$year,
-               prev=.)
+dir.names <- c("fit-us-transmission-50iter-10000N", "fit-us-transmission-50iter-5000N", "fit-us-transmission-50iter-1000N", "fit-us-transmission-50iter-500N", "test_fit_transmission")
 
-data.under.5.N <- filter(data.env$swab.data, age_range %in% levels(age_range)[1:5]) %>%
-    group_by(year) %>%
-    summarize_at("frequency", sum)
+estim.prev <- lapply(paste0(dir.names, "/trial-0/estimated_prevalences.csv"), read.csv)
 
-data.positive.under.5 <- filter(data.env$serotype.data, Age_Years %in% levels(Age_Years)[1:5]) %>%
-group_by(Collection_Year) %>%
-summarize_at("Number_of_Serotypes", sum)
-
-data.prev.under.5 <- mutate(data.under.5.N, prevalence=data.positive.under.5$Number_of_Serotypes/frequency)
-
-## Read in simulations
-
-sim.dir <- "test_US_simulations"
-
-trial.ids <- list.dirs(sim.dir, full.names=FALSE, recursive=FALSE) %>%
-    grep("trial-", ., value=TRUE) %>%
-    gsub("trial-", "", .) %>%
-    as.numeric()
+estim.prev.df <- data.frame(states=10:49,
+                            sapply(estim.prev, function (x) x[10:49, ncol(x)]))
 
 
-num.hosts.under.5 <- lapply(paste0(sim.dir, "/trial-", trial.ids, "/num_hosts_by_age.csv"), read.csv) %>%
-    lapply(select, 1:5) %>%
-    lapply(rowSums) %>%
-    as.data.frame
 
-simdata <- lapply(paste0(sim.dir, "/trial-", trial.ids, "/num_colonized_under_5.csv"), read.csv) %>%
-    as.data.frame() %>%
-    `/`(num.hosts.under.5) %>%
-    mutate(mean.prev=rowSums(.)/ncol(.)) %>%
-    data.frame(year=2011-nrow(.):1, .) %>%
-    slice(-1:-100) %>%
-    reshape2::melt(id.vars=c("year", "mean.prev"), variable.name="replicate") %>%
-    mutate(replicate=as.factor(as.numeric(replicate)))
+ggplot(reshape2::melt(estim.prev.df, id.vars="states")) + theme_bw() +
+  geom_density(aes(x=value, fill=variable), alpha=.4)
 
-P <- ggplot(simdata) + theme_bw() +
-  stat_summary(aes(x=year, y=value), fun.ymin = min, fun.ymax = max, geom="errorbar") +
-  geom_point(aes(x=year, y=value, colour=replicate), alpha=.6) +
-  geom_point(data=data.prev.under.5, aes(x=year, y=prevalence), colour="red") +
-  scale_colour_manual(values=rep("black", length(levels(simdata$replicate)))) +
-  theme(legend.position="none", text=element_text(size=14)) +
-xlab("Year") + ylab("Prevalence among children < 5") +
-scale_x_continuous(breaks=c(2001, 2003, 2005, 2007, 2009), limits=c(2000, 2010))
-ggsave("test_US_sim_prev.pdf", P, width=5, height=3.3)
+comparison <- data.frame(N=c(10000, 5000, 1000, 500, 250),
+           var=sapply(estim.prev.df[, -1], var),
+           time=c(95, 40, 12, 10, 9))
+
+ggplot(comparison%>%reshape2::melt(id.vars="N")) + theme_bw() + 
+  geom_point(aes(x=N, y=value)) +
+  geom_smooth(aes(x=N, y=value), method="glm", method.args=list(family="Gamma"), se=FALSE) +
+  facet_wrap(~variable, ncol=1, scales='free_y')
+
+# Each 10% increase in time, results in a 20% reduction in variance
+summary(lm(var~time, data=comparison%>%mutate(var=var/max(var), time=(time-min(time))/min(time))))
